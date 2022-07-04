c48e433a5db9ea858e89b77c8b75bc89167626f8
Danielxcott <changwilliam826@gmail.com>
2022-07-04 13:58:15 +0630

    added search data

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const searchInput = document.querySelector(".search-input");
 const submitBtn = document.querySelector(".submit-btn");
 const auth = "563492ad6f91700001000001892eefa7ed1e43e6b99daa205d9da68d"
 
-const curatedPhoto = async () =>{
-    const fetchData = await fetch(`https://api.pexels.com/v1/curated?per_page=15&page=1`,{
+const fetchApi = async(url)=>{
+    const fetchData = await fetch(url,{
         method:'GET',
         headers:{
             Accept : 'application/json',
@@ -12,16 +12,44 @@ const curatedPhoto = async () =>{
         }
     })
     const data = await fetchData.json();
-    // console.log(Math.floor(Math.random(data.photos.length)*15));
-    console.log(data.photos)
+    return data;
+}
+const getData = (data) =>{
     data.photos.forEach((photo)=>{
         const galleryDiv = document.createElement("div");
         galleryDiv.classList.add("gallery-img");
-        galleryDiv.innerHTML = `<img src=${photo.src.large} alt="" />
+        galleryDiv.innerHTML = `
+        <div class="gallery-info">    
+        <a href=${photo.src.original} download>Download</a>
         <p>${photo.photographer}</p>
+        </div>
+        <img src=${photo.src.large} alt="" />
         ` 
         gallery.appendChild(galleryDiv);
     })
 }
 
+const curatedPhoto = async () =>{
+    const data = await fetchApi(`https://api.pexels.com/v1/curated?per_page=15&page=1`);
+    // console.log(Math.floor(Math.random(data.photos.length)*15));
+    getData(data);
+}
+
 curatedPhoto();
+
+const search = async (searchText) =>{
+    clear();
+   const data = await fetchApi(`https://api.pexels.com/v1/search?query=${searchText}&per_page=15&page=1`);
+   getData(data);
+}
+
+const clear = ()=>{
+    searchInput.value="";
+    gallery.innerHTML = "";
+}
+
+submitBtn.addEventListener("click",(e)=>{
+    e.preventDefault();
+    const text = searchInput.value;
+    search(text);
+})
