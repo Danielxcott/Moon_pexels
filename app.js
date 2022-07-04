31f9e1ad9b8265370145de8cf7de4da0aee94176
Danielxcott <changwilliam826@gmail.com>
2022-07-04 17:05:16 +0630

    added more photo feature

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
 const gallery = document.querySelector(".gallery");
 const searchInput = document.querySelector(".search-input");
 const submitBtn = document.querySelector(".submit-btn");
-const auth = "563492ad6f91700001000001892eefa7ed1e43e6b99daa205d9da68d"
+const auth = "563492ad6f91700001000001892eefa7ed1e43e6b99daa205d9da68d";
+const nav = document.querySelector("nav");
+const iconSearch = document.querySelector(".icon");
+const moreBtn = document.querySelector(".more");
+let page = 1;
+let fetchMore;
+let currentSearch;
+
+const height  = window.innerHeight;
+
+window.addEventListener("scroll",function(){
+    const currentHeight = Math.floor(this.window.scrollY);
+    if(currentHeight>height-180){
+        nav.classList.add("scroll-nav")
+    }else{
+        nav.classList.remove("scroll-nav")
+    }
+})
 
 const fetchApi = async(url)=>{
     const fetchData = await fetch(url,{
@@ -30,7 +47,8 @@ const getData = (data) =>{
 }
 
 const curatedPhoto = async () =>{
-    const data = await fetchApi(`https://api.pexels.com/v1/curated?per_page=15&page=1`);
+     fetchMore = `https://api.pexels.com/v1/curated?per_page=15&page=1`;
+    const data = await fetchApi(fetchMore);
     // console.log(Math.floor(Math.random(data.photos.length)*15));
     getData(data);
 }
@@ -39,7 +57,8 @@ curatedPhoto();
 
 const search = async (searchText) =>{
     clear();
-   const data = await fetchApi(`https://api.pexels.com/v1/search?query=${searchText}&per_page=15&page=1`);
+     fetchMore = `https://api.pexels.com/v1/search?query=${searchText}&per_page=15&page=1`;
+   const data = await fetchApi(fetchMore);
    getData(data);
 }
 
@@ -52,4 +71,23 @@ submitBtn.addEventListener("click",(e)=>{
     e.preventDefault();
     const text = searchInput.value;
     search(text);
+    currentSearch = text;
 })
+
+iconSearch.addEventListener("click",()=>{
+    searchInput.classList.toggle("active");
+})
+
+const loadMorePhoto = async () =>{
+    page++;
+    if(currentSearch){
+        fetchMore = `https://api.pexels.com/v1/search?query=${currentSearch}&per_page=15&page=${page}`;
+    }else{
+        fetchMore = `https://api.pexels.com/v1/curated?per_page=15&page=${page}`;
+    }
+    const data = await fetchApi(fetchMore);
+    getData(data);
+}
+
+moreBtn.addEventListener("click",loadMorePhoto);
+
